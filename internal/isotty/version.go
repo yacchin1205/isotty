bec747b3c889982d7eb0137c0f35d1a87b5c67d6
yacchin1205 <968739+yacchin1205@users.noreply.github.com>
2026-05-14 17:30:42 +0900

    Prefer release version over Go build info

--- internal/isotty/version.go
+++ internal/isotty/version.go
@@ -5,6 +5,9 @@ import "runtime/debug"
 var defaultVersion = "dev"
 
 func Version() string {
+	if defaultVersion != "" && defaultVersion != "dev" {
+		return defaultVersion
+	}
 	if info, ok := debug.ReadBuildInfo(); ok {
 		if info.Main.Version != "" && info.Main.Version != "(devel)" {
 			return info.Main.Version
